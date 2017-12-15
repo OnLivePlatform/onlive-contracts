40b00be656974790de330d711f9efe428818f8c4
Jakub Stefanski <code@jakubstefanski.com>
2017-12-15 16:59:40 +0100

    Document OnLiveToken specification

--- contracts/OnLiveToken.sol
+++ contracts/OnLiveToken.sol
@@ -9,6 +9,12 @@ import { DescriptiveToken } from "./token/DescriptiveToken.sol";
 /**
  * @title OnLive Token
  * @author Jakub Stefanski
+ * @dev Implements ERC20 interface
+ * @dev Mintable by selected addresses until sale finishes
+ * @dev A cap on total supply of tokens
+ * @dev Burnable by anyone
+ * @dev Manual lock-up period (non-transferable) with a non-reversible release by the selected address
+ * @dev Modifiable symbol and name in case of collision
  */
 contract OnLiveToken is DescriptiveToken, ReleasableToken, CappedMintableToken, BurnableToken {
 
