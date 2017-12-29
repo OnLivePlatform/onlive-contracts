f9f90254fd36526838259972b264bfd9c3d867f7
Jakub Stefanski <code@jakubstefanski.com>
2017-12-29 12:04:16 +0100

    Add token and external crowdsale migrations

---
+++ migrations/3_deploy_external_crowdsale.ts
@@ -0,0 +1,20 @@
+import { OnLiveArtifacts } from 'onlive';
+import { Deployer } from 'truffle';
+import { toMillionsONL } from '../utils';
+
+declare const artifacts: OnLiveArtifacts;
+
+const OnLiveToken = artifacts.require('./OnLiveToken.sol');
+const ExternalCrowdsale = artifacts.require('./ExternalCrowdsale.sol');
+
+async function deploy(deployer: Deployer) {
+  const token = await OnLiveToken.deployed();
+  const tokensAvailable = toMillionsONL(5);
+  await deployer.deploy(ExternalCrowdsale, token.address, tokensAvailable);
+}
+
+function migrate(deployer: Deployer) {
+  deployer.then(() => deploy(deployer));
+}
+
+export = migrate;
