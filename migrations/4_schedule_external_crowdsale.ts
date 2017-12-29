2e8f70e4b1ddc4e6307aabe710a499af5874e01f
Jakub Stefanski <code@jakubstefanski.com>
2017-12-29 14:33:07 +0100

    Add external crowdsale schedule migration

---
+++ migrations/4_schedule_external_crowdsale.ts
@@ -0,0 +1,30 @@
+import { OnLiveArtifacts } from 'onlive';
+import { Deployer } from 'truffle';
+import * as Web3 from 'web3';
+
+import { daysToBlocks, hoursToBlocks, Web3Utils } from '../utils';
+
+declare const artifacts: OnLiveArtifacts;
+declare const web3: Web3;
+
+const utils = new Web3Utils(web3);
+
+const ExternalCrowdsale = artifacts.require('./ExternalCrowdsale.sol');
+
+async function deploy() {
+  const duration = daysToBlocks(11);
+  const startOffset = hoursToBlocks(1);
+
+  const currentBlock = await utils.getBlockNumber();
+  const startBlock = currentBlock + startOffset;
+  const endBlock = startBlock + duration;
+
+  const crowdsale = await ExternalCrowdsale.deployed();
+  await crowdsale.scheduleSale(startBlock, endBlock);
+}
+
+function migrate(deployer: Deployer) {
+  deployer.then(() => deploy());
+}
+
+export = migrate;
