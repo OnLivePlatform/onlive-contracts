3572aeb0c1d518c7d35e078608f781a4ef1b4b8a
Jakub Stefanski <code@jakubstefanski.com>
2018-01-17 11:49:42 +0100

    Make truffle network configs common

--- truffle-config.ts
+++ truffle-config.ts
@@ -1,26 +1,35 @@
 import { fromGwei } from './utils';
 
+const gas = 3500000;
+const gasPrice = fromGwei(52);
+const host = 'localhost';
+const port = 8545;
+
+const defaults = {
+  gas,
+  gasPrice,
+  host,
+  port
+};
+
 export = {
   networks: {
     mainnet: {
+      ...defaults,
       from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
-      gasPrice: fromGwei(21),
-      host: 'localhost',
-      network_id: '1',
-      port: 8545
+      network_id: '1'
     },
     rinkeby: {
+      ...defaults,
       from: '0x6c14a683e3e894fbEE5F929C5fAC402dF79694f6',
-      gasPrice: fromGwei(20),
-      host: 'localhost',
-      network_id: '4',
-      port: 8545
+      network_id: '4'
     },
     testrpc: {
+      gas,
       gasPrice: 0,
-      host: 'localhost',
+      host,
       network_id: '*',
-      port: 8545
+      port
     }
   }
 };
